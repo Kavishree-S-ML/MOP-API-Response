16946998b8fb690d6beaac446858815af4ee4322
KavishreeShanmugam <kavishree.s@maplelabs.com>
2017-06-13 12:38:44 +0530

    Create ansible_logs.js

--- ansible_logs.js
+++ ansible_logs.js
@@ -1,3 +1,5 @@
+//Get the last updated log statements from the activity_log file
+
 const electron = require('electron')
 const fs = require('fs');
 var direct_name = require('./config').morph_path;
@@ -8,15 +10,21 @@ var getActivitylog = function(page_num) {
     var log_data = {};
     var log_cont = [];
 
+//check the existence of the file path
     if (fs.existsSync(path)) {
+        //read the activity_log file
         var doc = (fs.readFileSync(path, 'utf8'));
         var x = page_num;
+
+        //convert the datas in the file into string
         var array = doc.toString().split(/(?:\r\n|\r|\n)/g);
         console.log("Activity Logs : \n-------------\n" + array)
+        //get only the latest updated statements(last updated 10 lines) from the file data
         for (var i = ((array.length) - ((10 * x) - 10)); i >= ((array.length) - (10 * x)); i--) {
             logs_det[i] = (array[i]);
         }
 
+        //store it in json format and return the response to the ui
         var content = {
             "Logs": logs_det
         };
