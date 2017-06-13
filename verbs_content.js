61e0101fd8d6bc8586bef4910f85182eac48e439
KavishreeShanmugam <kavishree.s@maplelabs.com>
2017-06-13 14:24:22 +0530

    Create verbs_content.js

--- verbs_content.js
+++ verbs_content.js
@@ -1,15 +1,18 @@
-var fs = require('fs'),
-    yaml = require('js-yaml'),
+//get the content in the verb yaml file
+var fs = require('fs'),  //package rquired to read/write the file 
+    yaml = require('js-yaml'),  //package required to read the yaml file content and change to json format
     direct_name = require('./config').morph_path;
 
 var getVerbDetail  = function(verbnameorid) {
     var verb_path = direct_name + "/roles/" + verbnameorid + "/tasks/main.yml";
     var mainPath = direct_name + "/roles/" + verbnameorid;
-    var doc = {};
+
     if (fs.existsSync(mainPath)) {
+        //read the Verb file
         var doc = yaml.safeLoad(fs.readFileSync(verb_path, 'utf8'));
         console.log("\n Verb : " + verbnameorid + "\n ------------------------------\n" + JSON.stringify(doc));
 
+        //store the content in json format and return it to the ui
         var content = {
             "Verb": verbnameorid,
             "Details": doc
