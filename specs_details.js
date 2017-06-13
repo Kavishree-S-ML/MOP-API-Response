c556b54e9faf71dbd85af2ac3ac6a2f8960fdac7
KavishreeShanmugam <kavishree.s@maplelabs.com>
2017-06-13 14:22:52 +0530

    Create specs_details.js

--- specs_details.js
+++ specs_details.js
@@ -1,13 +1,16 @@
-const fs = require('fs')
-var direct_name = require('./config').morph_path;
+//get the content in the specs file
+var fs = require('fs'),  //package rquired to read/write the file 
+    direct_name = require('./config').morph_path;
 
 var getVerbInParam = function(specName) {
     console.log("specName :" + specName);
     var main_Path = direct_name + '/specs/' + specName + '.json';
     if (fs.existsSync(main_Path)) {
+        //read the specs file
         var doc = (fs.readFileSync(main_Path, 'utf8'));
         console.log("\n Specs(VerbInParam) : "+specName+"\n ----------------------------------------\n" + (doc));
 
+        //store the content in json format and return it to the ui
         var content = {
             "Verb": specName,
             "Detail": JSON.parse(doc)
